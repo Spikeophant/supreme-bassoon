3aa47006dd7f41aab6c82be1a6ccd3539d1b0c76
Dustin Davisson <dustin.davisson@gmail.com>
2022-07-19 16:38:11 -0700

    Create user object, close issue #2.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//initialize user object to use throughout.
+
+var userPassword = {
+  pwLength: 0,
+  hasLower: false,
+  hasUpper: false,
+  hasNum: false,
+  hasSC: false,
+  password: ""
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
