846ae0bb8e355e8c19ee2d7700751a68a90fec4e
Dustin Davisson <dustin.davisson@gmail.com>
2022-07-19 17:47:26 -0700

    Gather bools for Lower case letters, upper case letters, numbers, and special characters and store them in the object for use at password generation time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,21 @@ var userPassword = {
       // if it's not right let's try until lit is.
       pwl = prompt("Your value was not correct, please enter a length from 8 to 128 in numbers.");
     }
+
     //set the value here for use throughout.
     this.pwLength = pwl;
+
+    // set the value for lower, upper, num, and sc to the results of the alert popup.  We'll validate after.
+    this.hasLower = confirm("Click okay to add lower case letters to your password.");
+    this.hasUpper = confirm("Click okay to add upper case letters to your password.");
+    this.hasNum = confirm("Click okay to add numbers to your password.");
+    this.hasSC = confirm("Click okay to add special chars to your password. ex: !@#$");
+
+    //validate the inputs above
+    if (!this.hasLower && !this.hasUpper && !this.hasNum && !this.hasSC) {
+      alert("You failed to add any character types to your password character pool, please click Generate password " +
+      "Again and click okay on at least one character type.")
+    }
   }
 }
 
